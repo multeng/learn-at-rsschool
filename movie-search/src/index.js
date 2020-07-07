7e69f56263602e52cddd12ec5f6d7e8e6f14c4ee
Vladimir Kormakov <cenizo.kht@gmail.com>
2020-07-07 13:56:36 +0300

    refactor: fixed functions

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -1,145 +1,114 @@
-import './styles/main.scss';
-import Swiper from 'swiper';
-import makeFilmPage from './js/filmPageWrapper';
-import translate from './js/translate';
+import "./styles/main.scss";
+import Swiper from "swiper";
+import makeFilmPage from "./js/filmPageWrapper";
+import translate from "./js/translate";
 
-
-const searchInput = document.querySelector('.search__input');
-const buttonSearch = document.querySelector('.search__button');
-const searchStatus = document.querySelector('.search-status__content');
-const swiperButtonNext = document.querySelector('.swiper-button-next');
-const form = document.querySelector('.search__form');
-let currentMovie = 'love';
+const searchInput = document.querySelector(".search__input");
+const buttonSearch = document.querySelector(".search__button");
+const searchStatus = document.querySelector(".search-status__content");
+const swiperButtonNext = document.querySelector(".swiper-button-next");
+const form = document.querySelector(".search__form");
+let currentMovie = "love";
 let currentPage = 1;
 
+const mySwiper = new Swiper(".swiper-container", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  initialSlide: 0,
+  simulateTouch: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    740: {
+      slidesPerView: 2,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 4,
+    },
+  },
+});
 
 function RenderMovieSet(movieDataSet) {
-    movieDataSet.forEach(async (movie) => {
-        const rating = await getMovieIMDB(movie.imdbID);
-        if (movie.Poster === 'N/A') {
-            movie.Poster = './src/assets/images/no-image.jpg';
-        }
-        const moviePage = makeFilmPage(movie.Poster, movie.Title, rating, movie.Year, movie.imdbID);
-        mySwiper.appendSlide(moviePage);
-        mySwiper.update();
-    })
-}
-
-
-
-function handler() {
-    buttonSearch.addEventListener('click', async () => {
-        mySwiper.removeAllSlides();
-        const movie = searchInput.value.toString();
-        if (movie.match(/[а-яё]/i)) {
-            currentMovie = await translate(movie);
-        } else {
-            currentMovie = movie;
-        }
-        let movieSet = await getMovieSet(currentMovie, currentPage);
-        if (!movieSet) {
-            searchStatus.innerText = 'Something went wrong...';
-            currentMovie = 'death';
-            let movieSet = await getMovieSet(currentMovie, currentPage);
-            RenderMovieSet(movieSet);
-        } else {
-            searchStatus.innerText = 'We found: ';
-            RenderMovieSet(movieSet);
-        }
-    })
-
-    swiperButtonNext.addEventListener('click', async () => {
-        if (mySwiper.slides.length - mySwiper.realIndex === 6) {
-            currentPage++;
-            let movieSet = await getMovieSet(currentMovie, currentPage);
-            RenderMovieSet(movieSet);
-        }
-    })
-
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        mySwiper.removeAllSlides();
-        const movie = searchInput.value.toString();
-        if (movie.match(/[а-яё]/i)) {
-            currentMovie = await translate(movie);
-        } else {
-            currentMovie = movie;
-        }
-        let movieSet = await getMovieSet(currentMovie, currentPage);
-        if (!movieSet) {
-            searchStatus.innerText = 'Something went wrong...';
-            currentMovie = 'death';
-            let movieSet = await getMovieSet(currentMovie, currentPage);
-            RenderMovieSet(movieSet);
-        } else {
-            searchStatus.innerText = 'We found: ';
-            RenderMovieSet(movieSet);
-        }
-    })
-}
-
-
-
-
-async function getMovieSet(movie, page) {
-    const url = `https://www.omdbapi.com/?s=${movie}&page=${page}&apikey=59ed6b54`;
-
-    const res = await fetch(url);
-    const data = await res.json();
-    if (data.Response === "False") {
-        return false;
+  movieDataSet.forEach(async (movie) => {
+    movie.rating = await getMovieIMDB(movie.imdbID);
+    if (movie.Poster === "N/A") {
+      movie.Poster = "./src/assets/images/no-image.jpg";
     }
-    return data.Search;
+    const moviePage = makeFilmPage(movie);
+    mySwiper.appendSlide(moviePage);
+  });
+  mySwiper.update();
 }
 
-async function getMovieIMDB(imdbID) {
-    const url = `https://www.omdbapi.com/?i=${imdbID}&apikey=59ed6b54`;
-
-    const res = await fetch(url);
-    const data = await res.json();
-    return data.imdbRating;
+async function renderNewMovieSlides(event) {
+  event.preventDefault();
+  mySwiper.removeAllSlides();
+  currentMovie = searchInput.value.toString();
+  if (currentMovie.match(/[а-яё]/i)) {
+    currentMovie = await translate(currentMovie);
+  }
+  let movieSet = await getMovieSet(currentMovie, currentPage);
+  if (!movieSet) {
+    searchStatus.innerText = "Something went wrong...";
+    currentMovie = "death";
+    movieSet = await getMovieSet(currentMovie, currentPage);
+  } else {
+    searchStatus.innerText = "We found: ";
+  }
+  RenderMovieSet(movieSet);
+  searchInput.value = "";
 }
 
-const mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    initialSlide: 0,
-    simulateTouch: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 1
-        },
-        740: {
-            slidesPerView: 2
-        },
-        1100: {
-            slidesPerView: 3
-        },
-        1440: {
-            slidesPerView: 4
-        }
+function addButtonsHandlers() {
+  buttonSearch.addEventListener("click", renderNewMovieSlides);
+
+  swiperButtonNext.addEventListener("click", async () => {
+    if (mySwiper.slides.length - mySwiper.realIndex === 6) {
+      currentPage++;
+      const movieSet = await getMovieSet(currentMovie, currentPage);
+      RenderMovieSet(movieSet);
     }
-})
+  });
+  form.addEventListener("submit", renderNewMovieSlides);
+}
 
-handler();
+function getMovieSet(movie, page) {
+  const url = `https://www.omdbapi.com/?s=${movie}&page=${page}&apikey=59ed6b54`;
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => (data.Response === "False" ? null : data.Search));
+}
+
+function getMovieIMDB(imdbID) {
+  const url = `https://www.omdbapi.com/?i=${imdbID}&apikey=59ed6b54`;
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data.imdbRating);
+}
+
+addButtonsHandlers();
 async function preload(searchMovie, page) {
-    currentMovie = searchMovie;
-    const movieSet = await getMovieSet(searchMovie, page);
+  currentMovie = searchMovie;
+  const movieSet = await getMovieSet(searchMovie, page);
+  RenderMovieSet(movieSet);
+  if (mySwiper.realIndex === 4) {
+    currentPage++;
+    let movieSet = await getMovieSet(movie, currentPage);
     RenderMovieSet(movieSet);
-    if (mySwiper.realIndex === 4) {
-        currentPage++;
-        let movieSet = await getMovieSet(movie, currentPage);
-        RenderMovieSet(movieSet);
-    }
+  }
 }
 preload(currentMovie, currentPage);
-window.addEventListener('resize', function (event) {
-    document.location.reload(true);
-})
+window.addEventListener("resize", function (event) {
+  document.location.reload(true);
+});
--- movie-search/src/js/filmPageWrapper.js
+++ movie-search/src/js/filmPageWrapper.js
@@ -1,23 +1,25 @@
-export default function makeFilmPage(imgUrl, filmTitle, rating, year, imdb) {
-    const wrapper = document.createElement('div');
-    wrapper.classList.add('swiper-slide');
-    wrapper.innerHTML +=`
+// export default function makeFilmPage(imgUrl, filmTitle, rating, year, imdb) {
+export default function makeFilmPage(movie) {
+  const { Poster, Title, Year, imdbID, rating } = movie;
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("swiper-slide");
+  wrapper.innerHTML += `
     <div class="card-container">
         <div class="poster">
-            <img src="${imgUrl}" alt="${filmTitle}">
+            <img src="${Poster}" alt="${Title}">
         </div>
         <div class="movie-title">
-            <a class="movie-tilte__link" target="_blank" href="https://www.imdb.com/title/${imdb}"><p class="movie-title__content">${filmTitle}</p></a>
+            <a class="movie-tilte__link" target="_blank" href="https://www.imdb.com/title/${imdbID}"><p class="movie-title__content">${Title}</p></a>
         </div>
         <div class="additional-info">
             <div class="year">
-                <p class="year__content">${year}</p>
+                <p class="year__content">${Year}</p>
             </div>
             <div class="rating">
                 <img src="./src/assets/images/star.png" alt="star">${rating}
             </div>
         </div>
     </div>
-    `
-    return wrapper;
-};
+    `;
+  return wrapper;
+}
